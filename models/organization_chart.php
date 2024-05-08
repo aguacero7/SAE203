12815ee347db3106a3e2d1581eadcbadc5e0cfb6
Enorme beau gosse <donaldinhodtp@gmail.com>
2024-05-09 00:30:09 +0100

    Update organization_chart.php

--- models/organization_chart.php
+++ models/organization_chart.php
@@ -9,6 +9,7 @@ checkPermissions($_SESSION["user"]);
 $title = "Annuaire";
 $script="../js/users.js";
 $get=$_GET;
+
 require_once("../controllers/controller_organization_chart.php");
 require_once("../templates/vue_chart.php");
 require_once("../templates/layout.php");
