2358fd755b05b846dc44a681f251a786d9dee5a9
Enorme beau gosse <donaldinhodtp@gmail.com>
2024-05-10 00:12:52 +0100

    Update vue_chart.php

--- templates/vue_chart.php
+++ templates/vue_chart.php
@@ -1,7 +1,6 @@
 <?php ob_start(); ?>
 <div class="container-fluid">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Annuaire des salariés</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
