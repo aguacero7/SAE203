ae885914301412674d9a87e41992811a8a78d77c
Aguacero <72492241+aguacero7@users.noreply.github.com>
2024-05-22 11:12:26 +0200

    Created HTTP response codes

--- models/classes/Error.php
+++ models/classes/Error.php
@@ -1,8 +1,28 @@
 <?php 
 
 class Error{
-    protected $code;
-    public function __contruct(int $code,string $page="", string $texte="",Exception $error=null){
+    private $code;
+    private $log_text;
+    private $log_file /*= PATH*/ ;
+    private $error_type;
+    public const HTTP_CODES = [
+        'OK' => 200,
+        'CREATED' => 201,
+        'NO_CONTENT' => 204,
+        'BAD_REQUEST' => 400,
+        'UNAUTHORIZED' => 401,
+        'FORBIDDEN' => 403,
+        'NOT_FOUND' => 404,
+        'METHOD_NOT_ALLOWED' => 405,
+        'INTERNAL_SERVER_ERROR' => 500,
+        'NOT_IMPLEMENTED' => 501,
+        'BAD_GATEWAY' => 502,
+        'SERVICE_UNAVAILABLE' => 503,
+    ];
+    /*TODO function logit(){
+
+    }*/
+    public function __contruct(int $code,  string $page="",   string $texte="",  Exception    $error=null){
         $this->code = $code;
         //TODO : log l'erreur 
 
@@ -11,15 +31,20 @@ class Error{
 
 }
 class RedirectedError extends Error{
-    protected $page;
-    public function __contruct(int $code,string $page, string $texte="",Exception $error=null){
+    private $page;
+    public function __contruct(int $code=Error::HTTP_CODES["FORBIDDEN"],    string $page,    string $texte="",   Exception $error=null){
+        parent::__construct($code, $page); // Appel du constructeur parent
         http_response_code($this->code);
         header("Location: ".$this->page);
+        
     }
 }
 class ErrorPage extends Error{
     public $texte;
-    public function __contruct(int $code,string $page="", string $texte= "",Exception $error=null){
+    private $error_type = "ERROR_PAGE";
+
+    public function __contruct(int $code,string $page="", string $texte,Exception $error=null){
+        parent::__construct($code, $texte, $error); // Appel du constructeur parent
         http_response_code($this->code);
         $this->texte=$texte;
         require_once("../templates/vue_error.php");
@@ -27,6 +52,5 @@ class ErrorPage extends Error{
 }
 class InternalError extends Error{
     public function __contruct(int $code,string $page= "", string $texte= "",Exception $error=null){
-
     }
 }
