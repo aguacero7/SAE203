1ee18eeb4b4e752bde33a853498117dc850c6261
Aguacero <72492241+aguacero7@users.noreply.github.com>
2024-05-22 10:28:54 +0200

    Update Error.php
    Added Error children classes

--- models/classes/Error.php
+++ models/classes/Error.php
@@ -2,9 +2,9 @@
 
 class Error{
     protected $code;
-    public function __contruct(int $code,string $page, string $texte){
+    public function __contruct(int $code,string $page="", string $texte="",Exception $error=null){
         $this->code = $code;
-        //TODO : log l'erreur
+        //TODO : log l'erreur 
 
     }
     
@@ -12,16 +12,21 @@ class Error{
 }
 class RedirectedError extends Error{
     protected $page;
-    public function __contruct(int $code,string $page, string $texte){
+    public function __contruct(int $code,string $page, string $texte="",Exception $error=null){
         http_response_code($this->code);
         header("Location: ".$this->page);
     }
 }
 class ErrorPage extends Error{
     public $texte;
-    public function __contruct(int $code,string $page, string $texte){
+    public function __contruct(int $code,string $page="", string $texte= "",Exception $error=null){
         http_response_code($this->code);
         $this->texte=$texte;
         require_once("../templates/vue_error.php");
+    }
+}
+class InternalError extends Error{
+    public function __contruct(int $code,string $page= "", string $texte= "",Exception $error=null){
+
     }
 }
