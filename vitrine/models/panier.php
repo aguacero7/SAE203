077a614aecc6e127d571b1f6dc828877ebef6267
Senseiroro <romaindavy2307@gmail.com>
2024-06-04 15:41:43 +0200

    ad panier.php
    ad panier.php and logo

---
+++ vitrine/models/panier.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Big K</title>
+    <link href="../style/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../style/bootstrap/js/bootstrap.min.js" rel="stylesheet">
+</head>
+
+<body>
+<div class="bg-success text-white">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <a class="navbar-brand text-white" href="vitrine.php">Big K</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="navbar-brand" href="panier.php">
+                    <img src="/img/panier.png" alt="" style="width:40px;">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+<?php
