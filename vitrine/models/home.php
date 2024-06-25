8a25dfd54508dae25addbfad908fcbbc1594538b
Senseiroro <romaindavy2307@gmail.com>
2024-06-25 23:58:48 +0200

    Modification vitrine
    MOdification de la page home.php et de la page menu.php et rajout de 2 images dans la branche vitrine

--- vitrine/models/home.php
+++ vitrine/models/home.php
@@ -1,110 +1,47 @@
 <?php $title = "Home";?>
 
-<div class="row row-cols-1 row-cols-md-3 g-4">
-    <div class="col">
-        <div class="card bg-dark text-white">
-            <h2>Tacos 1 viande</h2>
-            <p>Choisissez une viandes :</p>
-            <form id="commandeForm">
-                <label><input class="form-check-input" type="checkbox" id="check1" name="viande" value="boeuf"> Boeuf</label>
-                <label><input class="form-check-input" type="checkbox" id="check2" name="viande" value="poulet"> Poulet</label>
-                <label><input class="form-check-input" type="checkbox" id="check3" name="viande" value="kebab"> kebab</label>
-                <p>Choisissez des frites :</p>
-                <label><input class="form-check-input" type="radio" id="check4" name="frites" value="frites-classiques"> Frites classiques</label>
-                <label><input class="form-check-input" type="radio" id="check5" name="frites" value="frites-chedar"> Frites chedar</label>
-                <p>Choisissez une boisson :</p>
-                <label><input class="form-check-input" type="radio" id="check6" name="boisson" value="eau"> Eau</label>
-                <label><input class="form-check-input" type="radio" id="check7" name="boisson" value="coca"> Coca </label>
-                <label><input class="form-check-input" type="radio" id="check8" name="boisson" value="coca"> Ice Tea </label>
-                <label><input class="form-check-input" type="radio" id="check9" name="boisson" value="coca"> Orangina </label>
-                <label><input class="form-check-input" type="radio" id="check10" name="boisson" value="coca"> Fanta </label>
-                <input type="submit" class="btn btn-success" value="Commander">
-            </form>
-        </div>
-    </div>
 
-    <div class="col">
-        <div class="card bg-dark text-white">
-            <h2>Tacos 2 viandes</h2>
-            <p>Choisissez deux viandes :</p>
-            <form id="commandeForm">
-                <label><input class="form-check-input" type="checkbox" id="check1" name="viande" value="boeuf"> Boeuf</label>
-                <label><input class="form-check-input" type="checkbox" id="check2" name="viande" value="poulet"> Poulet</label>
-                <label><input class="form-check-input" type="checkbox" id="check3" name="viande" value="kebab"> kebab</label>
-                <p>Choisissez des frites :</p>
-                <label><input class="form-check-input" type="radio" id="check4" name="frites" value="frites-classiques"> Frites classiques</label>
-                <label><input class="form-check-input" type="radio" id="check5" name="frites" value="frites-chedar"> Frites chedar</label>
-                <p>Choisissez une boisson :</p>
-                <label><input class="form-check-input" type="radio" id="check6" name="boisson" value="eau"> Eau</label>
-                <label><input class="form-check-input" type="radio" id="check7" name="boisson" value="coca"> Coca </label>
-                <label><input class="form-check-input" type="radio" id="check8" name="boisson" value="coca"> Ice Tea </label>
-                <label><input class="form-check-input" type="radio" id="check9" name="boisson" value="coca"> Orangina </label>
-                <label><input class="form-check-input" type="radio" id="check10" name="boisson" value="coca"> Fanta </label>
-                <input type="submit" class="btn btn-success" value="Commander">
-            </form>
-        </div>
-    </div>
-
-    <div class="col">
-        <div class="card bg-dark text-white">
-            <h2>Tacos 3 viandes</h2>
-            <p>Choisissez trois viandes :</p>
-            <form id="commandeForm">
-                <label><input class="form-check-input" type="checkbox" id="check1" name="viande" value="boeuf"> Boeuf</label>
-                <label><input class="form-check-input" type="checkbox" id="check2" name="viande" value="poulet"> Poulet</label>
-                <label><input class="form-check-input" type="checkbox" id="check3" name="viande" value="kebab"> kebab</label>
-                <p>Choisissez des frites :</p>
-                <label><input class="form-check-input" type="radio" id="check4" name="frites" value="frites-classiques"> Frites classiques</label>
-                <label><input class="form-check-input" type="radio" id="check5" name="frites" value="frites-chedar"> Frites chedar</label>
-                <p>Choisissez une boisson :</p>
-                <label><input class="form-check-input" type="radio" id="check6" name="boisson" value="eau"> Eau</label>
-                <label><input class="form-check-input" type="radio" id="check7" name="boisson" value="coca"> Coca </label>
-                <label><input class="form-check-input" type="radio" id="check8" name="boisson" value="coca"> Ice Tea </label>
-                <label><input class="form-check-input" type="radio" id="check9" name="boisson" value="coca"> Orangina </label>
-                <label><input class="form-check-input" type="radio" id="check10" name="boisson" value="coca"> Fanta </label>
-                <input type="submit" class="btn btn-success" value="Commander">
-            </form>
-            <script>
-                // Écouteur d'événement pour le formulaire
-                document.getElementById('commandeForm').addEventListener('submit', function(event) {
-                    // Empêche le comportement par défaut du formulaire (rafraîchissement de la page)
-                    event.preventDefault();
-
-                    // Récupère les données du formulaire
-                    const formData = new FormData(event.target);
-
-                    // Convertit les données du formulaire en objet JavaScript
-                    const commandeObject = {};
-                    formData.forEach((value, key) => {
-                        commandeObject[key] = value;
-                    });
-
-                    // Convertit l'objet JavaScript en chaîne JSON
-                    const commandeJson = JSON.stringify(commandeObject, null, 2);
-
-                    // Enregistre la chaîne JSON dans un fichier
-                    const blob = new Blob([commandeJson], { type: 'application/json' });
-                    const url = URL.createObjectURL(blob);
-                    const a = document.createElement('a');
-                    a.href = url;
-                    a.download = 'commande.txt';
-                    a.click();
-                });
-                </script>
-            <p id="message"></p>
-            <script src="script.js"></script>
-        </div>
-    </div>
+<div class="container-fluid">
+    <h2> Bienvenue sur le site du Big K </h2>
+        <p>    
+        Big K est une entreprise de restauration rapide qui a révolutionné le concept des tacos à Saint-Malo. Fondée en 2021 par cinq passionnés de nourriture : Lilian, Antoine, Gaëtan, Noé et Romain, 
+        le Big K propose une expérience gastronomique unique avec ses tacos artisanaux. 
+        </p>
+        <p>
+        L'entreprise se distingue par son engagement envers la qualité des ingrédients et la créativité des saveurs. 
+        Chaque taco est conçu avec des produits frais locaux, des fromages variés et des sauces maison qui captivent les papilles des amateurs de tacos à travers la ville.
+        Big K a rapidement gagné en popularité grâce à son ambiance accueillante et son service rapide. 
+        </p>
+        <p>
+        L'entreprise a également mis en place un site internet pour faciliter les commandes à emporter et les réservations pour les clients qui préfèrent manger sur place.
+        Avec un esprit d'équipe fort et une passion pour la nourriture, Lilian, Antoine, Gaëtan, Noé et Romain continuent d'innover et de satisfaire les goûts exigeants de leurs clients. Big K est bien plus qu'un simple restaurant de tacos ; c'est un lieu où la communauté se rassemble autour de la bonne nourriture.
+        </p>
+        <p>
+        Pour découvrir les dernières créations de Big K et pour commander vos tacos préférés, n'hésitez pas à visiter leur site web ou leur boutique physique à Saint-Malo.
 </div>
-
-</div>
-            </div>
-        </div>
-        <div class="container bg-dark text-white">        
-        <h2 class="text-center my-5">Localisation</h2>
-        
-        <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1234.5678901234567!2d-1.987174!3d48.6582371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480e8349143218df:0xfb5a23e824f9e950!2sBig+K!5e0!3m2!1sfr!2sfr!4v1671234567890!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>        </div>
-        </div>
-        </div>
+<div id="carouselExample" class="carousel slide" data-bs-ride="Menu disponible">
+  <ol class="carousel-indicators">
+    <li data-bs-target="#carouselExample" data-bs-slide-to="0" class="active"></li>
+    <li data-bs-target="#carouselExample" data-bs-slide-to="1"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/tacos.jpg" class="d-block w-100 h-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/kebab.jpg" class="d-block w-100 h-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+    </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExample" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExample" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </a>
 </div>
--- vitrine/models/menu.php
+++ vitrine/models/menu.php
@@ -44,105 +44,70 @@
 -->
 
 
-<div class="row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
-    <div class="card bg-dark text-white" style="text-align: center; vertical-align: middle;">
-        <h2>Tacos 3 viandes</h2>
-        <p>Choisissez trois viandes :</p>
-        <form id="commandeForm">
-            <label><input class="form-check-input" type="checkbox" id="check1" name="viande" value="boeuf"> Boeuf</label>
-            <label><input class="form-check-input" type="checkbox" id="check2" name="viande" value="poulet"> Poulet</label>
-            <label><input class="form-check-input" type="checkbox" id="check3" name="viande" value="kebab"> kebab</label>
-            <p>Choisissez des frites :</p>
-            <label><input class="form-check-input" type="radio" id="check4" name="frites" value="frites-classiques"> Frites classiques</label>
-            <label><input class="form-check-input" type="radio" id="check5" name="frites" value="frites-chedar"> Frites chedar</label>
-            <p>Choisissez une boisson :</p>
-            <label><input class="form-check-input" type="radio" id="check6" name="boisson" value="eau"> Eau</label>
-            <label><input class="form-check-input" type="radio" id="check7" name="boisson" value="coca"> Coca </label>
-            <label><input class="form-check-input" type="radio" id="check8" name="boisson" value="coca"> Ice Tea </label>
-            <label><input class="form-check-input" type="radio" id="check9" name="boisson" value="coca"> Orangina </label>
-            <label><input class="form-check-input" type="radio" id="check10" name="boisson" value="coca"> Fanta </label>
-            <input type="submit" class="btn btn-success" value="Commander">
-        </form>
-        <script>
-            // Écouteur d'événement pour le formulaire
-            document.getElementById('commandeForm').addEventListener('submit', function(event) {
-                // Empêche le comportement par défaut du formulaire (rafraîchissement de la page)
-                event.preventDefault();
-
-                // Récupère les données du formulaire
-                const formData = new FormData(event.target);
-
-                // Convertit les données du formulaire en objet JavaScript
-                const commandeObject = {};
-                formData.forEach((value, key) => {
-                    commandeObject[key] = value;
-                });
-
-                // Convertit l'objet JavaScript en chaîne JSON
-                const commandeJson = JSON.stringify(commandeObject, null, 2);
-
-                // Enregistre la chaîne JSON dans un fichier
-                const blob = new Blob([commandeJson], { type: 'application/json' });
-                const url = URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = 'commande.txt';
-                a.click();
-            });
-            </script>
-        <p id="message"></p>
-        <script src="script.js"></script>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+        <div class="card bg-dark text-white">
+            <h2>Tacos 1 viande</h2>
+            <p>Choisissez une viandes :</p>
+            <form id="commandeForm">
+                <label><input class="form-check-input" type="checkbox" id="check1" name="viande" value="boeuf"> Boeuf</label>
+                <label><input class="form-check-input" type="checkbox" id="check2" name="viande" value="poulet"> Poulet</label>
+                <label><input class="form-check-input" type="checkbox" id="check3" name="viande" value="kebab"> kebab</label>
+                <p>Choisissez des frites :</p>
+                <label><input class="form-check-input" type="radio" id="check4" name="frites" value="frites-classiques"> Frites classiques</label>
+                <label><input class="form-check-input" type="radio" id="check5" name="frites" value="frites-chedar"> Frites chedar</label>
+                <p>Choisissez une boisson :</p>
+                <label><input class="form-check-input" type="radio" id="check6" name="boisson" value="eau"> Eau</label>
+                <label><input class="form-check-input" type="radio" id="check7" name="boisson" value="coca"> Coca </label>
+                <label><input class="form-check-input" type="radio" id="check8" name="boisson" value="coca"> Ice Tea </label>
+                <label><input class="form-check-input" type="radio" id="check9" name="boisson" value="coca"> Orangina </label>
+                <label><input class="form-check-input" type="radio" id="check10" name="boisson" value="coca"> Fanta </label>
+                <input type="submit" class="btn btn-success" value="Commander">
+            </form>
         </div>
     </div>
+
     <div class="col">
-    <div class="card bg-dark text-white" style="text-align: center; vertical-align: middle;">
-        <h2>Tacos 3 viandes</h2>
-        <p>Choisissez trois viandes :</p>
-        <form id="commandeForm">
-            <label><input class="form-check-input" type="checkbox" id="check1" name="viande" value="boeuf"> Boeuf</label>
-            <label><input class="form-check-input" type="checkbox" id="check2" name="viande" value="poulet"> Poulet</label>
-            <label><input class="form-check-input" type="checkbox" id="check3" name="viande" value="kebab"> kebab</label>
-            <p>Choisissez des frites :</p>
-            <label><input class="form-check-input" type="radio" id="check4" name="frites" value="frites-classiques"> Frites classiques</label>
-            <label><input class="form-check-input" type="radio" id="check5" name="frites" value="frites-chedar"> Frites chedar</label>
-            <p>Choisissez une boisson :</p>
-            <label><input class="form-check-input" type="radio" id="check6" name="boisson" value="eau"> Eau</label>
-            <label><input class="form-check-input" type="radio" id="check7" name="boisson" value="coca"> Coca </label>
-            <label><input class="form-check-input" type="radio" id="check8" name="boisson" value="coca"> Ice Tea </label>
-            <label><input class="form-check-input" type="radio" id="check9" name="boisson" value="coca"> Orangina </label>
-            <label><input class="form-check-input" type="radio" id="check10" name="boisson" value="coca"> Fanta </label>
-            <input type="submit" class="btn btn-success" value="Commander">
-        </form>
-        <script>
-            // Écouteur d'événement pour le formulaire
-            document.getElementById('commandeForm').addEventListener('submit', function(event) {
-                // Empêche le comportement par défaut du formulaire (rafraîchissement de la page)
-                event.preventDefault();
+        <div class="card bg-dark text-white">
+            <h2>Tacos 2 viandes</h2>
+            <p>Choisissez deux viandes :</p>
+            <form id="commandeForm">
+                <label><input class="form-check-input" type="checkbox" id="check1" name="viande" value="boeuf"> Boeuf</label>
+                <label><input class="form-check-input" type="checkbox" id="check2" name="viande" value="poulet"> Poulet</label>
+                <label><input class="form-check-input" type="checkbox" id="check3" name="viande" value="kebab"> kebab</label>
+                <p>Choisissez des frites :</p>
+                <label><input class="form-check-input" type="radio" id="check4" name="frites" value="frites-classiques"> Frites classiques</label>
+                <label><input class="form-check-input" type="radio" id="check5" name="frites" value="frites-chedar"> Frites chedar</label>
+                <p>Choisissez une boisson :</p>
+                <label><input class="form-check-input" type="radio" id="check6" name="boisson" value="eau"> Eau</label>
+                <label><input class="form-check-input" type="radio" id="check7" name="boisson" value="coca"> Coca </label>
+                <label><input class="form-check-input" type="radio" id="check8" name="boisson" value="coca"> Ice Tea </label>
+                <label><input class="form-check-input" type="radio" id="check9" name="boisson" value="coca"> Orangina </label>
+                <label><input class="form-check-input" type="radio" id="check10" name="boisson" value="coca"> Fanta </label>
+                <input type="submit" class="btn btn-success" value="Commander">
+            </form>
+        </div>
+    </div>
 
-                // Récupère les données du formulaire
-                const formData = new FormData(event.target);
-
-                // Convertit les données du formulaire en objet JavaScript
-                const commandeObject = {};
-                formData.forEach((value, key) => {
-                    commandeObject[key] = value;
-                });
-
-                // Convertit l'objet JavaScript en chaîne JSON
-                const commandeJson = JSON.stringify(commandeObject, null, 2);
-
-                // Enregistre la chaîne JSON dans un fichier
-                const blob = new Blob([commandeJson], { type: 'application/json' });
-                const url = URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = 'commande.txt';
-                a.click();
-            });
-            </script>
-        <p id="message"></p>
-        <script src="script.js"></script>
+    <div class="col">
+        <div class="card bg-dark text-white">
+            <h2>Tacos 3 viandes</h2>
+            <p>Choisissez trois viandes :</p>
+            <form id="commandeForm">
+                <label><input class="form-check-input" type="checkbox" id="check1" name="viande" value="boeuf"> Boeuf</label>
+                <label><input class="form-check-input" type="checkbox" id="check2" name="viande" value="poulet"> Poulet</label>
+                <label><input class="form-check-input" type="checkbox" id="check3" name="viande" value="kebab"> kebab</label>
+                <p>Choisissez des frites :</p>
+                <label><input class="form-check-input" type="radio" id="check4" name="frites" value="frites-classiques"> Frites classiques</label>
+                <label><input class="form-check-input" type="radio" id="check5" name="frites" value="frites-chedar"> Frites chedar</label>
+                <p>Choisissez une boisson :</p>
+                <label><input class="form-check-input" type="radio" id="check6" name="boisson" value="eau"> Eau</label>
+                <label><input class="form-check-input" type="radio" id="check7" name="boisson" value="coca"> Coca </label>
+                <label><input class="form-check-input" type="radio" id="check8" name="boisson" value="coca"> Ice Tea </label>
+                <label><input class="form-check-input" type="radio" id="check9" name="boisson" value="coca"> Orangina </label>
+                <label><input class="form-check-input" type="radio" id="check10" name="boisson" value="coca"> Fanta </label>
+                <input type="submit" class="btn btn-success" value="Commander">
+            </form>
         </div>
     </div>
 </div>
